622d562858d60703122186815d5789ae1dd8b7fe
Anshuman Singh <anshumas@gmail.com>
2022-04-19 22:25:38 -0500

    local

---
+++ Mission365/ProgrammingQuestions/BinarySearch/p350.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace DataStructureAlgorithm.ProgrammingQuestions
+{
+    [TestClass]
+    public class p350
+    {
+        [TestMethod]
+        public void IntersectTest()
+        {
+            int[] num1 = new int[] { 1, 2, 3, 4 };
+            int[] num2 = new int[] { 1, 2 };
+            var n = Intersect(num1, num2);
+
+
+        }
+        public int[] Intersect(int[] nums1, int[] nums2)
+        {
+            var lnum = nums1.Length > nums2.Length ? nums2 : nums1;
+            var hnum = nums1.Length < nums2.Length ? nums2 : nums1;
+            Dictionary<int, int> lmap = new Dictionary<int, int>();
+
+
+            return nums1;
+
+        }
+    }
+
+
+}
